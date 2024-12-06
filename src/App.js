27bcac5600d5979fa08eab165fce396933dccd68
sunilgoyubi <sunil.sunnymon@go-yubi.com>
2024-12-06 11:57:46 +0530

    platform addded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin.jsx";
+import Platform from "./pages/Platform";
+import Home from "./pages/Home.jsx";
 
-import './App.css';
-import Admin from '../src/pages/Admin'
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
-      <Admin/>
-      
-
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/platform" element={<Platform />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
+
