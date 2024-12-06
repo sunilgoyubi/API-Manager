3ec9fa4dbbe14269e81191e8c597b3a58aa1902f
Thenmozhi-yubi <thenmozhi.kumar@go-yubi.com>
2024-12-06 12:14:30 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Admin from '../src/pages/Admin'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
 
       <Admin/>
+      <Home/>
       
 
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className=' flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold mb-8'>API manager</h1>
+      <div className='flex gap-6'>
+        {/* admin card */}
+        <Link to = "">
+        <div className='bg-blue-500 text-white w-60 h-40'>
+          <p className=''>Admin</p>
+        </div>
+        </Link>
+
+        {/* platform card */}
+        <Link to = "">
+        <div>
+          <p>Platform</p>
+        </div>
+        </Link>
+
+      </div>
     </div>
   )
 }
